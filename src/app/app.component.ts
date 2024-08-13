3d0f0715a711e7d04629c8d0b974e37cafdbce08
jowgaze <jonatha.biomon.hp@gmail.com>
2024-08-13 17:26:23 -0300

    fix: routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { CardPlayerComponent } from './shared/card-player/card-player.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SearchComponent } from './shared/search/search.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardPlayerComponent, HeaderComponent, SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { LastGameComponent } from './pages/last-game/last-game.component';
+
+export const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'lastgame', component: LastGameComponent
+  }
+];
