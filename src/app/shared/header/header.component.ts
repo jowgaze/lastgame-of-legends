bb408064b7a42eb04e434ea96579ac019316dfa7
jowgaze <jonatha.biomon.hp@gmail.com>
2024-08-19 15:10:54 -0300

    feat: add logo image

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -5,11 +5,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule, FormsModule, MatInputModule, MatFormFieldModule],
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule, FormsModule, MatInputModule, MatFormFieldModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
