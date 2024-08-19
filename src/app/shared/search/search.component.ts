efa03eb5e4786aefaa8b4fd669abfb3dc1aa65e9
jowgaze <jonatha.biomon.hp@gmail.com>
2024-08-19 15:11:08 -0300

    fix: data display

--- src/app/shared/search/search.component.ts
+++ src/app/shared/search/search.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { RequestPlayer } from '../../core/types/requestPlayer';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, FormsModule, RouterLink],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
-
+  public gameName: string = "";
+  public tagLine: string = "";
 }
