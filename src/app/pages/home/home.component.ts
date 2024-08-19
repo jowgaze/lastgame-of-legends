24de9b1ad8e9cc7b02b6d3cff9b475928c67bfdf
jowgaze <jonatha.biomon.hp@gmail.com>
2024-08-19 15:18:37 -0300

    fix: remove unnecessary things

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { Account } from './../../core/types/account';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { SearchComponent } from '../../shared/search/search.component';
 import { CardPlayerComponent } from '../../shared/card-player/card-player.component';
-import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 import { ContainerComponent } from '../../shared/container/container.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
+import { PlayerService } from '../../services/player.service';
+import { RequestPlayer } from '../../core/types/requestPlayer';
+import { Summoner } from '../../core/types/summoner';
+import { League } from '../../core/types/league';
+import { MatchInfo } from '../../core/types/matchInfo';
+import { Player } from '../../core/types/player';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, SearchComponent, CardPlayerComponent, ContainerComponent, RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, SearchComponent, CardPlayerComponent, ContainerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  ngOnInit(): void {
+    localStorage.clear();
+  }
 
 }
--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -1,7 +1,7 @@
 import { Account } from './../core/types/account';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 import { Observable } from 'rxjs';
 import { RequestPlayer } from '../core/types/requestPlayer';
 import { Summoner } from '../core/types/summoner';
@@ -12,30 +12,23 @@ import { MatchInfo } from '../core/types/matchInfo';
   providedIn: 'root'
 })
 export class PlayerService {
-
-  private apiUrlAccount = environment.apiUrlAccount;
-  private apiUrlSummoner = environment.apiUrlSummoner;
-  private apiUrlLeague = environment.apiUrlLeague;
   private apiKey: string = environment.apiKey;
 
   constructor(private httpClient: HttpClient) { }
 
   public getAccount(player: RequestPlayer): Observable<Account> {
     const http$ = `/${player.gameName}/${player.tagLine}?api_key=${this.apiKey}`;
-    console.log(http$);
     return this.httpClient.get<Account>('/riot/account/v1/accounts/by-riot-id' + http$);
   }
 
   public getSummoner(puuid: string): Observable<Summoner> {
     const http$ = `/${puuid}?api_key=${this.apiKey}`;
-    console.log(http$);
 
     return this.httpClient.get<Summoner>('/lol/summoner/v4/summoners/by-puuid' + http$);
   }
 
   public getLeague(id: string): Observable<League[]>{
     const http$ = `/${id}?api_key=${this.apiKey}`;
-    console.log(http$);
 
     return this.httpClient.get<League[]>('/lol/league/v4/entries/by-summoner' + http$);
   }
@@ -43,14 +36,12 @@ export class PlayerService {
 
   public getMatchId(puuid: string): Observable<string[]>{
     const http$ = `/${puuid}/ids?start=0&count=20&api_key=${this.apiKey}`;
-    console.log(http$);
 
     return this.httpClient.get<string[]>('/lol/match/v5/matches/by-puuid' + http$);
   }
 
   public getMatch(matchId: string): Observable<MatchInfo>{
     const http$ = `/${matchId}?api_key=${this.apiKey}`;
-    console.log(http$);
 
     return this.httpClient.get<MatchInfo>('/lol/match/v5/matches' + http$);
   }
